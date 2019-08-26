c46fbcabef999aba73d97327eccb669cf3038662
HaliimaR <rahimi.halima17@gmail.com>
2019-08-26 14:32:13 +0200

    zoeken op snelheid

--- src/app/resultaantal/resultaantal.component.ts
+++ src/app/resultaantal/resultaantal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
+import { OvertredingService } from '../service/overtreding.service';
 
 @Component({
   selector: 'app-resultaantal',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultaantalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpClient, private overtredingService: OvertredingService) { }
 
+  snelheidFormGroup: FormGroup;
+  snelheidlist = [];
+  zoeken = [];
+  snel;
   ngOnInit() {
+    this.httpService.get('./assets/overtred.json').subscribe(
+      data => {
+        this.snelheidlist = data as string[];   //fill the array with data
+        //console.log(this.opnamelist[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
+    this.snelheidFormGroup = new FormGroup({
+      snelheid: new FormControl(''),
+    });
+  }
+  gevondensnelheid() {
+    this.snel = this.snelheidFormGroup.value.snelheid;
+    this.zoeken = this.overtredingService.zoekenovertredingsnelheid(this.snel, this.snelheidlist);
+    console.log(this.zoeken);
   }
 
 }
--- src/app/service/overtreding.service.ts
+++ src/app/service/overtreding.service.ts
@@ -7,19 +7,28 @@ export class OvertredingService {
 
   constructor() { }
   private plaatslist: Array<string> = [];
+  private snelheidlist: Array<string> = [];
 
   zoekenOpnameplaats(plaats, lijstplaats): Array<string> {
     this.plaatslist = [];
     for (let pl of lijstplaats) {
-      
       if (plaats == pl.opnameplaats_straat ) {
-
         this.plaatslist.push(pl);
-        //let jsonstring = '{"name":' + pl.name + '; "type" :' + pl.type + '; "trainer" : ' + pok.trainer + '; "gender" : ' + pok.gender + '; "owned" : ' + pok.owned + '}';
       }
     }
     return this.plaatslist;
   }
 
-  
+  zoekenovertredingsnelheid(snelheid, lijstsnelheid): Array<string>{
+
+    this.snelheidlist = [];
+    for(let snel of lijstsnelheid){
+      if (snelheid <= snel.aantal_overtredingen_snelheid){
+        this.snelheidlist.push(snel);
+        //console.log(snel);
+      }
+    }
+    return this.snelheidlist;
+  }
+
 }
